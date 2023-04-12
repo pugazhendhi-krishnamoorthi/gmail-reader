a0a6be1e87f9750be62cdf7c0ecbdea8ef64452f
pugazhendhi krishnamoorthi <krishpugazh684@gmail.com>
2023-04-12 10:33:17 +0530

    gmail reader added

---
+++ src/main/java/com/Gmail/InternetAddress.java
@@ -0,0 +1,9 @@
+package com.Gmail;
+
+public class InternetAddress {
+
+	public InternetAddress(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/Gmail/MimeMessage.java
@@ -0,0 +1,26 @@
+package com.Gmail;
+
+import jakarta.websocket.Session;
+
+public class MimeMessage {
+
+	public MimeMessage(Session session) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setFrom(InternetAddress internetAddress) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHeader(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setText(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
